0d1fb7d05fdabdc52045ef7f8a985fdb88eb0660
Dom-Code <59179076+Dom-Code@users.noreply.github.com>
2023-12-07 15:31:37 -0600

    removed imports no longer needed

--- src/Pages/Account/Account.tsx
+++ src/Pages/Account/Account.tsx
@@ -1,9 +1,5 @@
-import { useContext, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import AccountTabs from '../../Components/AccountTabs/AccountTabs';
-import ValidationContext from '../../Context/ValidationContext';
-import useAxiosPrivate from '../../Hooks/useAxiosPrivate';
-import { AxiosResponse } from 'axios';
 
 export interface RegisterFormProps {
   userEmail: string;
@@ -15,28 +11,6 @@ export interface LoginProps {
 }
 
 const Account = () => {
-  const data = useContext(ValidationContext);
-  const nav = useNavigate();
-  const axiosPrivate = useAxiosPrivate();
-
-  // useEffect(() => {
-  //   if (!data.isValid) {
-  //     axiosPrivate
-  //       .get('/getUsersTodolists')
-  //       .then((response: AxiosResponse) => {
-  //         data.setIsValid(true);
-  //         data.setLists(response.data.lists);
-  //         data.setTodos(response.data.todos);
-  //         nav('/');
-  //       })
-  //       .catch(() => {
-  //         data.setIsValid(false);
-  //         data.setLists([]);
-  //         data.setTodos([]);
-  //       });
-  //   }
-  // }, []);
-
   return (
     <>
       <AccountTabs />
